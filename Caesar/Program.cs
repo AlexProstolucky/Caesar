8daca816aa9fb69a92bd7fa339342ecc1a74e3de
AlexProstolucky <alexprostolucky@gmail.com>
2023-03-31 19:34:46 +0300

    Добавьте файлы проекта.

---
+++ Caesar/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        char[] ukrAlphabet = {
+'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П',
+'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'ґ', 'д', 'е',
+'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+'ч', 'ш', 'щ', 'ь', 'ю', 'я', 'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П',
+'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'ґ', 'д', 'е',
+'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+'ч', 'ш', 'щ', 'ь', 'ю', 'я'
+};
+        string text;
+        string result = "";
+        Console.WriteLine("Напишіть речення, яке потрібно зашифрувати:");
+        text = Console.ReadLine();
+        Console.WriteLine("Вкажіть відступ(максимальний 33)");
+        int step = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < text.Length; i++)
+        {
+            for (int j = 0; j < 66; j++)
+            {
+                if (text[i] == ukrAlphabet[j] || text[i] == ' ') 
+                {
+                    result += ukrAlphabet[j + step+66];
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
